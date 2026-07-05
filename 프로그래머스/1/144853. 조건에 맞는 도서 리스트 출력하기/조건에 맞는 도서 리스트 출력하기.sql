08363457d8747fcb51ba05834f18ccba8b894bfa
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-07-06 02:17:45 +0900

    Update 조건에 맞는 도서 리스트 출력하기.sql

--- 프로그래머스/1/144853. 조건에 맞는 도서 리스트 출력하기/조건에 맞는 도서 리스트 출력하기.sql
+++ 프로그래머스/1/144853. 조건에 맞는 도서 리스트 출력하기/조건에 맞는 도서 리스트 출력하기.sql
@@ -1,8 +1,14 @@
 -- 코드를 입력하세요
 SELECT
-    BOOK_ID, PUBLISHED_DATE
+    BOOK_ID, 
+    DATE_FORMAT(PUBLISHED_DATE, '%Y-%m-%d') AS PUBLISHED_DATE
+    -- %Y는 4자리(2021) %y는 2자리(01) %m은 2자리(12) %d는 2자리(01)
 FROM
     BOOK
 WHERE 
-    PUBLISHED_DATE LIKE '2021%' AND CATEGORY = '인문'
-ORDER BY PUBLISHED_DATE;
+    -- 년도만 빼주는 날짜 전용 함수(YEAR, MONTH, DAY)
+    -- WHERE LEFT(PUBLISHED_DATE, 4) = '2021'도 가능
+    YEAR(PUBLISHED_DATE) = 2021 
+    AND CATEGORY = '인문'
+ORDER BY 
+    PUBLISHED_DATE;
