f775279b232feb726700d784f1bcb67925ea2724
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-07-30 20:28:43 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
@@ -1,38 +1,43 @@
 #include <iostream>
 #include <vector>
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int solution(vector<vector<int>> board)
 {
     int n = board.size();
     int m = board[0].size();
+    
+    // dp[i][j] = i,j를 우하단 꼭짓점으로 하는 가장 큰 정사각형의 한 변
+    vector<vector<int>> dp(n, vector<int>(m, 0));
+    
     int mx = 0;
     
-    vector<vector<int>> dp(n, vector<int>(m,0));
-    for(int i = 0; i < n; i++){ // 0열 채우기
+    for(int i = 0; i < n; i++){
         dp[i][0] = board[i][0];
-        mx = max(mx, dp[i][0]); // mx 초기값
+        mx = max(mx, dp[i][0]);
     }
-    for(int j = 0; j < m; j++){
+    
+    for(int j = 0;  j< m; j++){
         dp[0][j] = board[0][j];
         mx = max(mx, dp[0][j]);
     }
-    // 초기값 설정
+    
+    
+    
     for(int i = 1; i < n; i++){
         for(int j = 1; j < m; j++){
-            if(board[i][j] == 1){
-                dp[i][j] = min({dp[i-1][j],dp[i-1][j-1],dp[i][j-1]}) + board[i][j];
-            }
+            if(board[i][j] == 0) dp[i][j] = 0;
             else{
-                dp[i][j] = 0;
+                dp[i][j] = min({dp[i-1][j], dp[i-1][j-1], dp[i][j-1]}) + 1;
             }
         }
     }
 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
-            mx = max(mx,dp[i][j]);
+            mx = max(mx, dp[i][j]);
         }
     }
     
