28ce402a946a561d6af8f332b1a147b9ec432675
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-08-14 16:08:58 +0900

    [level 2] Title: 숫자 변환하기, Time: 3.86 ms, Memory: 7.32 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
@@ -1,35 +1,25 @@
 #include <vector>
 #include <queue>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int solution(int x, int y, int n) {
-    // x와 y가 같으면 시작하자마자 0 반환
-    if (x == y) return 0;
-    
-    vector<int> dist(y + 1, -1);
-    queue<int> q;
-    
-    q.push(x);
-    dist[x] = 0;
-    
-    while(!q.empty()){
-        int cur = q.front();
-        q.pop(); // 큐에서 빼는 위치는 보통 맨 앞쪽으로 빼두는 것이 안전하고 깔끔함.
+    vector<int> dp(y + 1, 1e9);
+    dp[x] = 0;
+    for(int i = x; i <= y; i++){
+        if(dp[i] == 1e9) continue;
         
-        vector<int> v = {cur + n, cur * 2, cur * 3};
-        
-        for(int next : v){
-            if(next > y) continue;         // 범위를 벗어났는지 먼저 확인
-            if(dist[next] != -1) continue; // 범위 안이라면, 방문했는지 확인
-            
-            // 정답을 찾았다면 큐에 넣을 필요 없이 바로 횟수 반환
-            if(next == y) return dist[cur] + 1;
-            
-            q.push(next);
-            dist[next] = dist[cur] + 1;
+        if(i + n <= y){
+            dp[i + n] = min(dp[i + n], dp[i] + 1);
+        }
+        if(i * 2 <= y){
+            dp[i * 2] = min(dp[i * 2], dp[i] + 1);
+        }
+        if(i * 3 <= y){
+            dp[i * 3] = min(dp[i * 3], dp[i] + 1);
         }
     }
     
-    return -1;
+    return dp[y] == 1e9 ? -1 : dp[y];
 }
