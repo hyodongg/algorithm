8276affeed1bd879e7d9e86d369f901d83f29210
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-08-17 18:09:27 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 3.42 ms, Memory: 45.1 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cpp
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cpp
@@ -5,37 +5,49 @@
 using namespace std;
 
 vector<vector<int>> board;
-int zero;
-int one;
 
-bool canQuard(int row, int col, int size){ // 격자판 숫자가 다 같은지
-    int num = board[row][col];
-    for(int i = row; i < row + size; i++){
-        for(int j = col; j < col + size; j++){
+int zero_cnt;
+int one_cnt;
+
+// 압축할 수 있는지
+bool canCompress(int r, int c, int size){
+    int num = board[r][c];
+    for(int i = r; i < r + size; i++){
+        for(int j = c; j < c + size; j++){
             if(num != board[i][j]) return false;
         }
     }
     return true;
-    
 }
 
-void dfs(int row, int col, int size){
-    if(canQuard(row, col, size)){
-        if(board[row][col] == 0) zero++;
-        else one++;
+void dfs(int r, int c, int size){
+    // 압축할 수 있는 경우 size가 1일때도 되므로 통일 됨
+    if(canCompress(r, c, size)){
+        // 숫자에 맞게 더하기
+        if(board[r][c] == 0) zero_cnt += 1;
+        if(board[r][c] == 1) one_cnt += 1;
         return;
     }
-    int half = size / 2;
-    dfs(row, col, half);
-    dfs(row + half, col, half);
-    dfs(row, col + half, half);
-    dfs(row + half, col + half, half);
+    
+    // 다음 나눠질 정사각형의 크기
+    int ns = size / 2;
+    
+    dfs(r, c, ns); // 왼쪽 위 사각형
+    dfs(r + ns, c, ns); // 왼쪽 아래 사각형
+    dfs(r, c + ns, ns); // 오른쪽 위 사각형
+    dfs(r + ns, c + ns, ns); // 오른쪽 아래 사각형
+    
 }
 
 vector<int> solution(vector<vector<int>> arr) {
     board = arr;
-    dfs(0,0, board.size());
-    vector<int> answer{zero, one};
+    zero_cnt = 0;
+    one_cnt = 0;
     
-    return answer;
+    int size = board.size();
+    
+    // 왼쪽 위 꼭짓점을 넣는다.
+    dfs(0, 0, size);
+    
+    return vector<int>{zero_cnt, one_cnt};
 }
